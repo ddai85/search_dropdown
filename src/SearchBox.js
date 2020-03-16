3dfc9abab8166227c025b5fe341c1778dd1f9cae
DanDai <ddai85@gmail.com>
2020-03-16 16:18:35 -0700

    Clean up create-react-app files

---
+++ src/SearchBox.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default class SearchBox extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p>This is searchbox</p>
+      </div>
+    );
+  }
+}
