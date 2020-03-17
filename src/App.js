e8b96b70cd75fbcee77dd0403367e8b7d8d7baab
DanDai <ddai85@gmail.com>
2020-03-16 18:03:50 -0700

    Implement searchbox component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React from "react";
 import SearchBox from "./SearchBox.js";
 
 function App() {
+  const searchTerms1 = ["credit card", "controller", "book", "shoes", "chair"];
+  const searchTerms2 = ["banana", "apple", "orange", "pear"];
+
   return (
     <div className="App">
-      <SearchBox />
+      <SearchBox searchTerms={searchTerms1} />
+      <SearchBox searchTerms={searchTerms2} />
     </div>
   );
 }
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,15 +1,78 @@
 import React from "react";
+import PropTypes from "prop-types";
+import "./SearchBox.css";
+
+// SearchBox Component receives an array of searchable strings from its parent component
+// If the user types a substring that matches one of the searchable strings in the array, a drop down will appear with all valid terms
+// Clicking on a search term will auto-fill the input box
+// Clicking outside of the input box will automatically close the drop down
 
 export default class SearchBox extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      searchList: [],
+      searchTerm: ""
+    };
+
+    this.handleLoseFocus = this.handleLoseFocus.bind(this);
+  }
+
+  handleChange(searchTerm) {
+    if (searchTerm === "") {
+      this.setState({ searchList: [], searchTerm: searchTerm });
+      return;
+    }
+    const regex = new RegExp(searchTerm, "gi");
+    const filteredList = this.props.searchTerms.filter(term => {
+      return term.match(regex);
+    });
+
+    this.setState({ searchList: filteredList, searchTerm: searchTerm });
+  }
+
+  handleLoseFocus() {
+    // Need a small delay in case searchterm is being clicked
+    setTimeout(() => {
+      this.setState({ searchList: [] });
+    }, 100);
   }
 
   render() {
+    let dropDown = null;
+
+    if (this.state.searchList.length) {
+      const searchTerms = this.state.searchList.map((term, index) => {
+        return (
+          <li key={index} onClick={() => this.handleChange(term)}>
+            {term}
+          </li>
+        );
+      });
+
+      dropDown = (
+        <div id="dropdown">
+          <ul>{searchTerms}</ul>
+        </div>
+      );
+    }
     return (
-      <div className="App">
-        <p>This is searchbox</p>
+      <div id="container">
+        <input
+          id="searchinput"
+          type="text"
+          onChange={e => this.handleChange(e.target.value)}
+          onFocus={e => this.handleChange(e.target.value)}
+          onBlur={this.handleLoseFocus}
+          value={this.state.searchTerm}
+        />
+        {dropDown}
       </div>
     );
   }
 }
+
+SearchBox.propTypes = {
+  searchTerms: PropTypes.array
+};
